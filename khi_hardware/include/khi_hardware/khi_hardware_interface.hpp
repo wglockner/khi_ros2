e25f2e7d577e64021c8de019414615960e40cca0
wglockner <walter.w.glockner@gmail.com>
2025-12-10 09:33:41 -0600

    hw update for jazzy

--- khi_hardware/include/khi_hardware/khi_hardware_interface.hpp
+++ khi_hardware/include/khi_hardware/khi_hardware_interface.hpp
@@ -19,7 +19,9 @@
 #include <string>
 #include <vector>
 
+#include "hardware_interface/hardware_info.hpp"
 #include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/types/hardware_component_interface_params.hpp"
 #include "hardware_interface/types/hardware_interface_type_values.hpp"
 #include "khi_hardware/khi_driver.hpp"
 #include "khi_hardware/khi_robot.hpp"
@@ -42,7 +44,7 @@ public:
 
   KHI_ROBOT_HARDWARE_PUBLIC
   hardware_interface::CallbackReturn on_init(
-    const hardware_interface::HardwareInfo & info) override;
+    const hardware_interface::HardwareComponentInterfaceParams & params) override;
 
   KHI_ROBOT_HARDWARE_PUBLIC
   hardware_interface::CallbackReturn on_configure(
@@ -92,6 +94,8 @@ private:
   bool is_cleaning_up_ = false;
   bool is_deactivating_ = false;
   bool is_shutdowning_ = false;
+  bool is_active_ = false;
+  hardware_interface::HardwareInfo info_;
   std::shared_ptr<KhiDriver> driver_;
   std::shared_ptr<KhiPublisher> publisher_;
   std::shared_ptr<KhiService> service_;
