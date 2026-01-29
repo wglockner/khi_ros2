702bf112923cb8bcdbd846f292d3a4277c3095ce
wglockner <walter.w.glockner@gmail.com>
2026-01-29 13:15:20 -0600

    Modifications for cx165l-bc01

--- khi_hardware/launch/khi_bringup.launch.py
+++ khi_hardware/launch/khi_bringup.launch.py
@@ -95,7 +95,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "robot_ip",
-            default_value="192.168.0.2",
+            default_value="192.168.2.23",
             description="ip address",
         )
     )
